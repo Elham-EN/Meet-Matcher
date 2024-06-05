c63af26539282f275bfbf69ab28e2ea7e38f65d9
Elham-EN <ilhamrezaie@gmail.com>
2024-06-05 17:47:31 +1000

    bootstrap Next.Js Application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from "@nextui-org/button";
+import { FaRegSmile } from "react-icons/fa";
+
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className=" text-2xl">Next.js Full Stack Application</h1>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-semibold">Next.js Full Stack Application</h1>
+      <Button startContent={<FaRegSmile size={20} />} color="primary">
+        Click Me
+      </Button>
     </div>
   );
 }
