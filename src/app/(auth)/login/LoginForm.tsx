d3df287b8a403e2a612f4494078b6243a3477e67
Elham-EN <ilhamrezaie@gmail.com>
2024-06-06 13:27:17 +1000

    integrated useform hook

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,11 +1,29 @@
+"use client";
 import FormInput from "@/components/form/FormInput";
-import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { GiPadlock } from "react-icons/gi";
 
+export type LoginFormType = {
+  email: string;
+  password: string;
+};
+
 export default function LoginForm(): React.ReactElement {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginFormType>();
+
+  const onSubmitHandler = (data: any) => {
+    console.log(data);
+    errors.email;
+  };
+
   return (
-    <Card className="w-2/5 mx-auto">
+    <Card className="w-2/5 min-w-[400px] mx-auto">
       <CardHeader className="flex flex-col item-center justify-center">
         <div className="flex flex-col gap-2 items-center text-pink-400">
           <div className="flex flex-row items-center gap-3">
@@ -16,11 +34,29 @@ export default function LoginForm(): React.ReactElement {
         </div>
       </CardHeader>
       <CardBody>
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
           <div className="space-y-4">
-            <FormInput label="Email" variant="bordered" />
-            <FormInput label="Password" variant="bordered" type="password" />
-            <Button fullWidth className="bg-pink-400 text-white text-lg" type="submit">
+            <FormInput
+              label="Email"
+              variant="bordered"
+              register={register}
+              fieldName="email"
+              error={errors.email}
+            />
+            <FormInput
+              label="Password"
+              variant="bordered"
+              type="password"
+              register={register}
+              fieldName="password"
+              error={errors.password}
+            />
+            <Button
+              fullWidth
+              className="bg-pink-400 text-white text-lg"
+              type="submit"
+              isDisabled={!isValid}
+            >
               Login
             </Button>
           </div>
--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { LoginFormType } from "@/app/(auth)/login/LoginForm";
 import { Input } from "@nextui-org/react";
 import React, { ReactElement, useState } from "react";
+import { FieldError, UseFormRegister } from "react-hook-form";
 import { BsFillEyeSlashFill } from "react-icons/bs";
 import { IoEyeSharp } from "react-icons/io5";
 
@@ -13,12 +15,18 @@ interface Props {
   label: React.ReactNode;
   variant: VariantType;
   type?: InputType;
+  fieldName: keyof LoginFormType;
+  register: UseFormRegister<LoginFormType>;
+  error?: FieldError | undefined;
 }
 
 export default function FormInput({
   label,
   variant,
   type = "text",
+  fieldName,
+  register,
+  error,
 }: Props): ReactElement {
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
@@ -39,10 +47,14 @@ export default function FormInput({
 
   return (
     <Input
+      defaultValue=""
       label={label}
       variant={variant}
       type={type === "password" ? (isVisible ? "text" : "password") : "text"}
       endContent={PasswordToggle}
+      {...register(fieldName, { required: `${label} is required` })}
+      isInvalid={!!error}
+      errorMessage={error?.message}
     />
   );
 }
