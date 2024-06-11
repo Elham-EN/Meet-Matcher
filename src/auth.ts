3e1cd7e03b29497db459ceee010b30c42167e09a
Elham-EN <ilhamrezaie@gmail.com>
2024-06-11 14:18:11 +1000

    populate the user's ID inside the session data

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,17 @@ import authConfig from "./auth.config";
 import { prisma } from "./libs/prisma";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  // Get executed based on events happening with authentication, such as
+  // we refesh the page or when we log in as a user
+  callbacks: {
+    // To populate the user's ID inside the session data
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
   // Database Adapters are the bridge we use to connect Auth.js to your database
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
