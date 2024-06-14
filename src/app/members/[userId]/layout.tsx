93add05286635ea65298689332ed59875f7e0c82
Elham-EN <ilhamrezaie@gmail.com>
2024-06-14 23:32:55 +1000

    created layout component

---
+++ src/app/members/[userId]/layout.tsx
@@ -0,0 +1,27 @@
+import { getMemberByUserId } from "@/app/actions/memberActions";
+import React, { ReactElement } from "react";
+import MemberSidebar from "../MemberSidebar";
+import { notFound } from "next/navigation";
+import { Card } from "@nextui-org/react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: { userId: string };
+}
+
+async function layout({ children, params }: LayoutProps): Promise<ReactElement> {
+  const member = await getMemberByUserId(params.userId);
+  if (!member) notFound();
+  return (
+    <div className="grid grid-cols-12 gap-5 h-[80vh]">
+      <div className="col-span-3">
+        <MemberSidebar member={member} />
+      </div>
+      <div className="col-span-9">
+        <Card className="w-full mt-10 h-[80vh]">{children}</Card>
+      </div>
+    </div>
+  );
+}
+
+export default layout;
--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -1,9 +1,9 @@
 import { getMemberByUserId } from "@/app/actions/memberActions";
 import { notFound } from "next/navigation";
 import React from "react";
-import MemberSidebar from "../MemberSidebar";
 
-// Dynamic Route
+// Dynamic Route. When we use layout page, this page going to be child of the
+// layout page
 async function MemberDetailsPage({ params }: { params: { userId: string } }) {
   const member = await getMemberByUserId(params.userId);
   if (!member) notFound();
@@ -11,7 +11,6 @@ async function MemberDetailsPage({ params }: { params: { userId: string } }) {
     <div>
       <h1>Member ID: {params.userId}</h1>
       <h1>Member Name: {member?.name}</h1>
-      <MemberSidebar member={member} />
     </div>
   );
 }
