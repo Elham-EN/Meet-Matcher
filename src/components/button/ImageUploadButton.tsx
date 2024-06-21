2fc1a9edb58f68fdb8c916847de94fba7e0d8772
Elham-EN <ilhamrezaie@gmail.com>
2024-06-21 22:42:11 +1000

    created customize cloduinary upload button

---
+++ src/components/button/ImageUploadButton.tsx
@@ -0,0 +1,24 @@
+import { CldUploadButton } from "next-cloudinary";
+import React, { ReactElement } from "react";
+import { HiPhoto } from "react-icons/hi2";
+
+/**
+ * creates a button element that uses an instance of the Cloudinary
+ * Upload Widget to give you an easy way to add upload capabilities
+ * to your Next.js app
+ */
+export default function ImageUploadButton(): ReactElement {
+  return (
+    <CldUploadButton
+      options={{ maxFiles: 1 }}
+      onSuccess={(res) => console.log(res)}
+      signatureEndpoint={"/api/sign-image"}
+      uploadPreset="nm-nextJS-app"
+      className="flex items-center gap-2 bg-pink-600 text-white 
+        rounded-lg py-2 px-2 hover:bg-pink-600/70"
+    >
+      <HiPhoto />
+      Upload new image
+    </CldUploadButton>
+  );
+}
